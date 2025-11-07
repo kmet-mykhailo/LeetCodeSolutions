21545462bd7fa526ba441696547b3741cb99ffb7
Kmet Mykhailo <kmetmisha@gmail.com>
2025-11-07 18:10:51 +0200

    Problem 790

---
+++ LeetcodeConsoleApp/Solutions/DynamicProgramming/790_Domino_And_Tromino_Tiling_Medium.cs
@@ -0,0 +1,33 @@
+namespace LeetcodeConsoleApp.Solutions.DynamicProgramming
+{
+    public class DominoAndTrominoTiling : ISolution
+    {
+        public void Run()
+        {
+            int[] inputs = [ 0, 5, 42 ];
+            foreach (var input in inputs)
+            {
+                var result = NumTilings(input);
+                Console.WriteLine($"Result {result}");
+            }
+        }
+
+        private static int NumTilings(int n)
+        {
+            const int MOD = 1000000007;
+            if (n <= 2) return n;
+
+            long[] dp = new long[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            dp[2] = 2;
+
+            for (int i = 3; i <= n; i++)
+            {
+                dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+            }
+
+            return (int)dp[n];
+        }
+    }
+}
