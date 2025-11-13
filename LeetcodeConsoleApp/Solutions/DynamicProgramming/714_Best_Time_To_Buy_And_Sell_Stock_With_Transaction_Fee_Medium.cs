ed3f03e96434562333f745e3a5398040b4ddfa7e
Kmet Mykhailo <kmetmisha@gmail.com>
2025-11-13 10:49:58 +0200

    updated 714

--- LeetcodeConsoleApp/Solutions/DynamicProgramming/714_Best_Time_To_Buy_And_Sell_Stock_With_Transaction_Fee_Medium.cs
+++ LeetcodeConsoleApp/Solutions/DynamicProgramming/714_Best_Time_To_Buy_And_Sell_Stock_With_Transaction_Fee_Medium.cs
@@ -25,43 +25,5 @@ namespace LeetcodeConsoleApp.Solutions.DynamicProgramming
 
             return maxProfit;
         }
-
-        private static int MaxProfit2(int[] prices, int fee)
-        {
-            int profit = 0;
-            int spentMoney = -prices[0];
-            Console.WriteLine($"SpentMoney:{spentMoney}");
-            Console.WriteLine($"Profit:{profit}");
-
-            for (int i = 0; i < prices.Length; i++)
-            {
-                spentMoney = Math.Max(spentMoney, profit - prices[i]);
-                Console.WriteLine($"SpentMoney:{spentMoney}");
-                profit = Math.Max(profit, spentMoney + prices[i] - fee);
-                Console.WriteLine($"Profit:{profit}");
-            }
-            return profit;
-        }
-
-        private static int MaxProfit1(int[] prices, int fee)
-        {
-            int result = 0;
-            int[][] temp = new int[prices.Length -1][];
-            int[] maxValues = new int[prices.Length - 1];
-            for (int i = 0; i < prices.Length-1; i++)
-            {
-                temp[i] = new int[prices.Length - 1 - i];
-                for (int j = 0; j < prices.Length - 1 - i; j++)
-                {
-                    temp[i][j] = prices[j+i+1] - prices[i];
-                    if(temp[i][j] > maxValues[i])
-                    {
-                        maxValues[i] = temp[i][j];
-                    }
-                }
-            }
-
-            return default;
-        }
     }
 }
