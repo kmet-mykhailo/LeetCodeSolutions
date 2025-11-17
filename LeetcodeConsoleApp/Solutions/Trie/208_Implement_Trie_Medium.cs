1cc47bdae9d8fcced2d7c6777144105ed62b9a56
Kmet Mykhailo <kmetmisha@gmail.com>
2025-11-17 15:54:24 +0200

    208

---
+++ LeetcodeConsoleApp/Solutions/Trie/208_Implement_Trie_Medium.cs
@@ -0,0 +1,85 @@
+﻿
+namespace LeetcodeConsoleApp.Solutions.Trie
+{
+    public class ImplementTrie : ISolution
+    {
+        public void Run()
+        {
+            Trie trie = new();
+            trie.Insert("apple");
+
+            bool result = trie.Search("apple");   // return True
+            Console.WriteLine(result);
+
+            result = trie.Search("app");     // return False
+            Console.WriteLine(result);
+
+            result = trie.StartsWith("app"); // return True
+            Console.WriteLine(result);
+
+            trie.Insert("app");
+            result =  trie.Search("app");     // return True
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Trie
+    {
+        private readonly Dictionary<char, Trie> childern = [];
+        private bool IsLeaf = false;
+
+        public Trie()
+        {
+
+        }
+
+        public void Insert(string word)
+        {
+            Trie current = this;
+            foreach(char c in word)
+            {
+                if (!current.childern.TryGetValue(c, out Trie? value))
+                {
+                    value = new Trie();
+                    current.childern[c] = value;
+                }
+
+                current = value;
+            }
+
+            current.IsLeaf = true;
+        }
+
+        public bool Search(string word)
+        {
+            Trie current = this;
+            foreach (char c in word)
+            {
+                if (!current.childern.TryGetValue(c, out Trie? value))
+                {
+                    return false;
+                }
+
+                current = value;
+            }
+
+            return current.IsLeaf;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            Trie current = this;
+            foreach (char c in prefix)
+            {
+                if (!current.childern.TryGetValue(c, out Trie? value))
+                {
+                    return false;
+                }
+
+                current = value;
+            }
+
+            return true;
+        }
+    }
+}
